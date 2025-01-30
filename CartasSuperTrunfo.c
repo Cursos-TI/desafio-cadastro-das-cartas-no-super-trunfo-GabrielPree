311f76c90b9fea77b7b4938efe67e351c0d79b11
Gabriel <gabriellopesp@hotmail.com>
2025-01-30 14:14:13 +0000

    Usei double para o pib e %lf para ter mais precisão

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,10 @@ int main() {
     int populacao;
     int ponto;
     float area;
-    float pib;
-    float pibpercapita;
+    double pib;
+    double pibpercapita;
     float densidade;
-
+    
     //Entrada de dados
     printf("Digite o nome da cidade: ");
     scanf(" %[^\n]", nome);//usei %[^\n] para que fosse possivel ler espaço no nome da cidade
@@ -18,21 +18,21 @@ int main() {
     printf("Digite a area da cidade: ");
     scanf("%f", &area);
     printf("Digite o PIB da cidade: ");
-    scanf("%f", &pib);
+    scanf("%lf", &pib);
     printf("Digite o numero de pontos turisticos da cidade: ");
     scanf("%d", &ponto);
 
     //Calculos
     pibpercapita = (float) pib / populacao;//usei casting para garantir a precisão e evitar perda de dados
     densidade = populacao / area;
-    
+
     //Saida de dados
     printf("Nome da Cidade: %s\n", nome);
     printf("População: %d\n", populacao);
     printf("Area: %.2f km²\n", area);
-    printf("PIB: %.2f\n", pib);
+    printf("PIB: %.2lf\n", pib);
     printf("Numero de pontos turisticos: %d\n", ponto);
-    printf("PIB per capita: %2.f\n", pibpercapita);
+    printf("PIB per capita: %.2lf\n", pibpercapita);
     printf("Densidade Populacional: %.2f hab/km²\n", densidade);
     return 0;
 
