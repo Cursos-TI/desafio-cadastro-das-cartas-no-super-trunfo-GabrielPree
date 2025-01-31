cb8436dc359f4320138215522a4cf523354d6ed6
Gabriel <gabriellopesp@hotmail.com>
2025-01-31 14:28:56 +0000

    Adicionei modularidade e mais comentarios

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-int main() {
-    //variaveis
+ //variaveis globais
     char nome[50];
     int populacao;
     int ponto;
@@ -9,8 +8,10 @@ int main() {
     double pib;
     double pibpercapita;
     float densidade;
-    
-    //Entrada de dados
+
+//Função genérica para entrada de dados
+void entradaDados() {
+    //código para a função entrada de Dados
     printf("Digite o nome da cidade: ");
     scanf(" %[^\n]", nome);//usei %[^\n] para que fosse possivel ler espaço no nome da cidade
     printf("Digite a população da cidade: ");
@@ -21,12 +22,18 @@ int main() {
     scanf("%lf", &pib);
     printf("Digite o numero de pontos turisticos da cidade: ");
     scanf("%d", &ponto);
-
-    //Calculos
+}
+ 
+// Função genérica para cálculos do PIB per capita e densidade populacional
+float calculos() {
+    // código para a função calculos
     pibpercapita = (float) pib / populacao;//usei casting para garantir a precisão e evitar perda de dados
     densidade = populacao / area;
-
-    //Saida de dados
+}
+ 
+// Função genérica para exibir dados
+void exibirDados() {
+    // código para a função exibirDados
     printf("Nome da Cidade: %s\n", nome);
     printf("População: %d\n", populacao);
     printf("Area: %.2f km²\n", area);
@@ -34,7 +41,12 @@ int main() {
     printf("Numero de pontos turisticos: %d\n", ponto);
     printf("PIB per capita: %.2lf\n", pibpercapita);
     printf("Densidade Populacional: %.2f hab/km²\n", densidade);
+}
+ 
+int main() {
+ 
+    entradaDados();
+    calculos();
+    exibirDados();
     return 0;
-
-
 }
